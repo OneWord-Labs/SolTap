ff5fe8b8347f0cf5b6dd445e4f58a0dc13876b7d
Aksel Owl <aksel@openowl.ai>
2024-12-28 16:16:35 -0800

    chore: migrate from Vercel to Render

--- src/config/telegram.config.ts
+++ src/config/telegram.config.ts
@@ -10,7 +10,7 @@ export const TELEGRAM_CONFIG = {
   botToken: process.env.TELEGRAM_BOT_TOKEN || '',
   gameShortName: 'solsays',
   webAppUrl: process.env.NODE_ENV === 'production' 
-    ? 'https://play.soltap.xyz'
+    ? 'https://soltap-v2.onrender.com'
     : process.env.BASE_URL || 'http://localhost:3001',
   botUrl: 'https://t.me/SolSays_bot',
 };
